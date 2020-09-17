75e23f96b1c0bc28b4ae82cd8dac1defb84eb6ad
friendlyanon <friendlyanon@users.noreply.github.com>
2020-09-17 16:54:30 +0000

    Add max_consecutive_ones package

---
+++ packages/max_consecutive_ones/source/main.cpp
@@ -0,0 +1,48 @@
+#include <fmt/core.h>
+
+#include <algorithm>
+#include <cstddef>
+#include <vector>
+
+constexpr auto is_one = [](int i) { return i == 1; };
+constexpr auto is_not_one = [](int i) { return !is_one(i); };
+
+[[nodiscard]] std::ptrdiff_t max_consecutive_ones(const std::vector<int>& numbers) {
+  auto begin = numbers.begin();
+  const auto end = numbers.end();
+  begin = std::find_if(begin, end, is_one);
+  if (begin == end) {
+    return {0};
+  }
+
+  std::ptrdiff_t result = 0;
+  for (;;) {
+    const auto previous = begin++;
+    begin = std::find_if(begin, end, is_not_one);
+    const bool at_end = begin == end;
+
+    const auto difference = begin - previous - (at_end ? 1 : 0);
+    if (difference > result) {
+      result = difference;
+    }
+
+    if (at_end) {
+      break;
+    }
+  }
+
+  return result;
+}
+
+int main() {
+  const std::vector numbers{1, 1, 0, 1, 1, 1};
+  const auto max_consecutive = max_consecutive_ones(numbers);
+
+  std::puts("Numbers: [");
+  for (int number : numbers) {
+    fmt::print("  {},\n", number);
+  }
+  std::puts("]");
+
+  fmt::print("Max number of consecutive ones: {}\n", max_consecutive);
+}
