be31e805ddd32fd9b6c141676bd0e9d2d079cc2d
friendlyanon <friendlyanon@users.noreply.github.com>
2020-09-17 12:45:01 +0000

    Refine workflows

--- scripts/all.sh
+++ scripts/all.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
 CWD="$(cd "$(dirname "$0")" && pwd)"
+TARGET="build/standalone"
 
-# release build is omitted on purpose
-$CWD/test.sh && $CWD/standalone.sh && $CWD/docs.sh
+cd $CWD/..
+
+configure() {
+  if [ -d $TARGET ]; then
+    return 0
+  fi
+
+  cmake -Hstandalone -B $TARGET -GNinja \
+    -DUSE_CCACHE:BOOL=ON \
+    -DCPM_SOURCE_CACHE:PATH="$(pwd)/.cpm-cache"
+}
+
+build() {
+  cmake --build $TARGET
+}
+
+configure && build
